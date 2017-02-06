39a9a0b8912ddbb9d2b991fb5cbc2b14915903dc
Ryo <kanadajinlee@yahoo.ca>
2017-02-06 13:20:26 -0500

    Common components are imported/exported in 1 line of code each in new index file, with import from app.js and ES6 magic

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import { Header } from './components/common'; //it will automatically search for "index" file, so you don't need to specify it ;)
 
 class App extends Component {
   render(){
